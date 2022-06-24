3f3a364fae8f9059a6c1b358231a471378aec8c9
Prashanta Chakraborty <prashanta0234@gmail.com>
2022-06-24 22:18:07 +0600

    add fs ethers

--- deploy.js
+++ deploy.js
@@ -1,5 +1,21 @@
+const { ethers } = require("ethers");
+const fs = require("fs");
+
 async function main() {
   // write code here
+  const provider = new ethers.providers.JsonRpcProvider(
+    "HTTP://127.0.0.1:7545"
+  );
+  const wallet = new ethers.Wallet(
+    "8f11fea45ebb3a9d4faff2a88019a2c47b80563b0a3b7b22dc27e43a110020c1",
+    provider
+  );
+  const abi = fs.readFileSync("SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync("SimpleStorage_sol_SimpleStorage.bin", "utf8");
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Your contact is deploying.....");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
